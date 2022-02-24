e50bb9d000039e1e9475948b3d44d36fb83c8385
Aaron Varga <apvarga@student.fullsail.edu>
2022-02-24 10:52:07 -0500

    footer updates

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -4,23 +4,41 @@ import styled from "styled-components";
 
 import MyLogo from "./MyLogo";
 
+import { FaCopyright } from "react-icons/fa";
+
 export default function MyFooter() {
   return (
     <FooterContainer>
       <h1>
         <MyLogo />
       </h1>
+      <Paragraph>
+        <FaCopyright style={{ marginRight: ".25rem", marginTop: ".25rem" }} /> Copyright 2022 Varga Lujos.<br />
+        All rights reserved.
+      </Paragraph>
     </FooterContainer>
   );
 }
 
 const FooterContainer = styled.footer`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  justify-content: center;
   background: #151515;
   border-radius: 0 0 .25rem .25rem;
   justify-content: center;
   @media (max-width: 768px) {
     border-radius: 0 0 47.33px 47.33px;
   }
+`;
+
+const Paragraph = styled.p`
+  display: flex;
+  color: #333;
+  justify-content: center;
+  margin: 0 0 1rem 0;
+  font-size: 1rem;
+  border-top: 1px solid #333;
+  padding-top: 1rem;
+  text-align: center;
 `;
