ec1f15e1cc5975598a957218cdd525abaee42181
Aaron Varga <apvarga@student.fullsail.edu>
2022-03-20 12:24:16 -0400

    Added router link to logo

--- src/components/MyLogo.js
+++ src/components/MyLogo.js
@@ -1,5 +1,5 @@
-import React, {  } from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import VargaLujosLogo from "../images/logo.png";
@@ -7,7 +7,9 @@ import VargaLujosLogo from "../images/logo.png";
 export default function MyLogo() {
   return (
     <LogoContainer>
-      <LogoImage src={VargaLujosLogo} alt="Varga Lujos Logo" />
+      <LogoLink to="/">
+        <LogoImage src={VargaLujosLogo} alt="Varga Lujos Logo" />
+      </LogoLink>
     </LogoContainer>
   );
 }
@@ -24,4 +26,8 @@ const LogoImage = styled.img`
   cursor: pointer;
   width: 100%;
   max-width: 1000px;
+`;
+
+const LogoLink = styled(Link)`
+  text-decoration: none;
 `;
